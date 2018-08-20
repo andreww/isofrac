6e68f6810fba564b0242a8b84b28106faee799f1
Andrew Walker <a.walker@leeds.ac.uk>
2018-08-20 13:30:26 +0100

    Update alpha_geoptherm for proposal
    Just want Br, not MgO for this.

--- alpha_geotherm_2.py
+++ alpha_geotherm_2.py
@@ -85,34 +85,34 @@ if __name__ == "__main__":
 
     # Volume of MgO
     MgO_eos = fit_PVT_EOS_params(
-        glob.glob('/Volumes/earawa/Castep-isotopes-work/free_energy/MgO/MgO_*GPa/MgO.castep'))
+        glob.glob('../free_energy/MgO/MgO_*GPa/MgO.castep'))
     MgO_Vs = MgO_eos(Ps, Ts)
     MgO_Vs_athermal = MgO_eos(Ps, np.zeros_like(Ts))
 
     # Volume of MgSiO3 Pv
     MgSiO3_eos = fit_PVT_EOS_params(
-        glob.glob('/Volumes/earawa/Castep-isotopes-work/free_energy/MgSiO3/MgSiO3_*GPa/MgSiO3.castep'))
+        glob.glob('../free_energy/MgSiO3/MgSiO3_*GPa/MgSiO3.castep'))
     MgSiO3_Vs = MgSiO3_eos(Ps, Ts)
     MgSiO3_Vs_athermal = MgSiO3_eos(Ps, np.zeros_like(Ts))
     
     # Volume of MgSiO3 Pv
     Mg2SiO4_eos = fit_PVT_EOS_params(
-        glob.glob('/Volumes/earawa/Castep-isotopes-work/free_energy/Mg2SiO4/Mg2SiO4_*GPa/Mg2SiO4.castep'))
+        glob.glob('../free_energy/Mg2SiO4/Mg2SiO4_*GPa/Mg2SiO4.castep'))
     Mg2SiO4_Vs = Mg2SiO4_eos(Ps, Ts)
     Mg2SiO4_Vs_athermal = Mg2SiO4_eos(Ps, np.zeros_like(Ts))
 
     # 1000.ln(beta) for MgO
-    MgO_beta_fun = fit_beta(glob.glob('/Volumes/earawa/Castep-isotopes-work/MgO_DFPT/MgO_prim222_900eV_k333_q444_DFPT_*GPa/MgO.castep'), supercell=True) 
+    MgO_beta_fun = fit_beta(glob.glob('../MgO_DFPT/MgO_prim222_900eV_k333_q444_DFPT_*GPa/MgO.castep'), supercell=True) 
     MgO_betas = MgO_beta_fun(Ts, MgO_Vs)
     MgO_betas_athermal = MgO_beta_fun(Ts, MgO_Vs_athermal)
 
     # 1000.ln(beta) for MgSiO3
-    MgSiO3_beta_fun = fit_beta(glob.glob('/Volumes/earawa/Castep-isotopes-work/MgSiO3_DFPT/MgSiO3_900eV_k333_q333_DFPT_*GPa/mgsio3.castep')) 
+    MgSiO3_beta_fun = fit_beta(glob.glob('../MgSiO3_DFPT/MgSiO3_900eV_k333_q333_DFPT_*GPa/mgsio3.castep')) 
     MgSiO3_betas = MgSiO3_beta_fun(Ts, MgSiO3_Vs)
     MgSiO3_betas_athermal = MgSiO3_beta_fun(Ts, MgSiO3_Vs_athermal)
 
     # 1000.ln(beta) for Mg2SiO3
-    Mg2SiO4_beta_fun = fit_beta(glob.glob('/Volumes/earawa/Castep-isotopes-work/Forsterite_DFPT/Fo_900ev_k424_q313_DFPT_*GPa/forsterite.castep')) 
+    Mg2SiO4_beta_fun = fit_beta(glob.glob('../Forsterite_DFPT/Fo_900ev_k424_q313_DFPT_*GPa/forsterite.castep')) 
     Mg2SiO4_betas = Mg2SiO4_beta_fun(Ts, Mg2SiO4_Vs)
     Mg2SiO4_betas_athermal = Mg2SiO4_beta_fun(Ts, Mg2SiO4_Vs_athermal)
 
@@ -125,20 +125,43 @@ if __name__ == "__main__":
     print "Done fitting... now plotting" 
 
     f, ax1 = plt.subplots()
-    ax1.set_xlabel(r"$\Delta^{}$Mg (per mill) relative to forsterite".format('{26}'))
-    ax1.set_ylabel("Depth (km)")
+
+    fs = 14
+    fs_l = fs
+
+    ax_depths = np.array([0, 200, 400, 600, 800, 1000, 1200, 1400])
+    ax_Ts, ax_Ps = depth_PT(ax_depths)
     ax1.invert_yaxis()
-    ax1.plot((Mg2SiO4_betas_athermal - MgO_betas_athermal), depths, 'b--')
-    ax1.plot((Mg2SiO4_betas - MgO_betas), depths, 'b-')
-    ax1.plot((Mg2SiO4_betas_athermal - MgSiO3_betas_athermal), depths, 'r--')
-    ax1.plot((Mg2SiO4_betas - MgSiO3_betas), depths, 'r-')
+    ax1.plot(ax_Ts, ax_Ps, ':g')
+    ax1.set_xlim(left=2000, right=3600)
+    ax1.set_xlabel("T (K)", fontsize=fs)
+    ax1.set_ylabel("P (GPa)", fontsize=fs)
+    ax1.tick_params(axis='both', which='both', labelsize=fs_l)
+
+    ax2 = ax1.twinx()
+    ax2.invert_yaxis()
+    ax2.plot(ax_Ts, ax_depths, alpha=0) # invisable
+    ax2.set_ylabel("Depth (km)", fontsize=fs)
+    ax2.tick_params(axis='both', which='both', labelsize=fs_l)
+    ax3 = ax2.twiny()
+    ax3.set_xlabel(r"$\Delta^{}$Mg (per mill) relative to forsterite".format('{26}'), fontsize=fs)
+    ax3.set_xlim(left=0.0, right=0.12)
+    ax3.tick_params(axis='both', which='both', labelsize=fs_l)
+    # ax1.plot((Mg2SiO4_betas_athermal - MgO_betas_athermal), depths, 'b--')
+    # ax1.plot((Mg2SiO4_betas - MgO_betas), depths, 'b-')
+    # ax1.plot((Mg2SiO4_betas_athermal - MgSiO3_betas_athermal), depths, 'r--')
+    ax3.plot((Mg2SiO4_betas - MgSiO3_betas), depths, 'r-')
 
     #ax2 = ax1.twinx()
-    #ax2_tick_ds = [200, 400, 600, 800, 1000]
+    #ax2_tick_ds = np.array([200, 400, 600, 800, 1000])
     #ax2_tick_Ps, ax2_tick_Ts = depth_PT(ax2_tick_ds)
     #ax2_tick_labs = ["200", "400", "600", "800", "1000"]
     #ax2.set_ylabel("P (GPa)")
     #ax2.set_yticks(ax2_tick_ds)
     #ax2.set_yticks(ax2_tick_labs)
+
+    f.tight_layout()
+
+    f.savefig("alpha_geotherm_2.pdf")
 
     plt.show()
